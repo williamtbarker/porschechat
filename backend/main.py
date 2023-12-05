7abc39a68778daba3ddfad07514cbec635a3601b
Will Barker <barkerwilliamt@gmail.com>
2023-12-04 23:41:06 -0500

    minor changes

--- backend/main.py
+++ backend/main.py
@@ -1,41 +1,98 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from dotenv import load_dotenv
 import os
+import json
+from sqlalchemy import create_engine, MetaData, Table, Column, String, Text
+import databases
+import redis
 from openai import OpenAI
 
-# Load environment variables
-load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
+# Environment Variables
+DATABASE_URL = os.getenv("DATABASE_URL")
+REDIS_URL = os.getenv("REDIS_URL")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-# Initialize the OpenAI client
-client = OpenAI(api_key=api_key)
+# Database Setup
+metadata = MetaData()
 
-# Initialize FastAPI app
+chat_history_table = Table(
+    "chat_history",
+    metadata,
+    Column("session_id", String, primary_key=True),
+    Column("conversation", Text)
+)
+
+engine = create_engine(DATABASE_URL)
+metadata.create_all(engine)
+
+database = databases.Database(DATABASE_URL)
+
+# Redis Setup
+redis_client = redis.from_url(REDIS_URL)
+
+# OpenAI Client
+openai_client = OpenAI(api_key=OPENAI_API_KEY)
+
+# FastAPI App
 app = FastAPI()
 
-# Define request model
+
+# Pydantic Models
 class ChatRequest(BaseModel):
     prompt: str
+    session_id: str
 
-# Define endpoint
-@app.post("/chat")
+
+class ChatResponse(BaseModel):
+    response: str
+
+
+# Endpoint to handle chat
+@app.post("/chat", response_model=ChatResponse)
 async def chat_with_openai(request: ChatRequest):
-    try:
-        response = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": request.prompt}
-            ]
-        )
-        return {"response": response.choices[0].message.content}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    session_id = request.session_id
+    session = redis_client.get(session_id)
 
-# Optional: Add health check endpoint
-@app.get("/health")
-async def health_check():
-    return {"status": "running"}
+    if not session:
+        async with database.transaction():
+            query = chat_history_table.select().where(chat_history_table.c.session_id == session_id)
+            result = await database.fetch_one(query)
+            session = json.loads(result.conversation) if result else []
+
+    session.append({"role": "user", "content": request.prompt})
+
+    # OpenAI call
+    response = openai_client.Completion.create(
+        model="gpt-3.5-turbo",
+        prompt=json.dumps(session),  # Ensure prompt format aligns with OpenAI's requirements
+        max_tokens=150
+    )
+    answer = response.choices[0].text.strip()
+    session.append({"role": "assistant", "content": answer})
+
+    # Update Redis and Postgres
+    redis_client.set(session_id, json.dumps(session))
+    async with database.transaction():
+        update_query = chat_history_table.update().where(chat_history_table.c.session_id == session_id).values(
+            conversation=json.dumps(session))
+        await database.execute(update_query)
+
+    return ChatResponse(response=answer)
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
 
 
